351e00b277656d0c5d72e99a09a6a495f979d55a
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2021-11-07 21:46:54 +0700

    Create Spinner component

---
+++ src/views/components/Spinner/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import style from "./Spinner.module.css";
+
+const Spinner: React.FC = () => {
+  return <div className={style.spinner} />;
+};
+
+export default Spinner;
